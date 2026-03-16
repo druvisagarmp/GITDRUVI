bcabe191c03824bdfce99e99547bbed0351329de
druvi <24maftpy0013@gmail.com>
2026-03-16 15:35:33 +0530

    Added student python file

---
+++ student.py
@@ -0,0 +1,8 @@
+name='druvi'
+course = 'MBA Fintech'
+year = 2
+college = "pondicherry university"
+print("student name : ", name)
+print("course name : ", course)
+print("no of years", year)
+print("name of college is : ",college)
---
+++ task.py
@@ -0,0 +1,30 @@
+shop_name = "Super Mart"
+item_name = "Notebook"
+item_price = 25.75
+item_count = 5
+
+print(f"""
+Shop Name: {shop_name}
+Item: {item_name}
+Price: {item_price}
+no of item : {item_count}
+""")
+
+print(type(item_name))
+print(type(item_price))
+print(type(item_count))
+(""
+"")
+
+radius = float(input("Enter radius of the circle: "))
+area = 3.14 * radius * radius
+print("Area of the circle is:", area)
+
+number = int(input("Enter a number: "))
+if number % 2 == 0:
+    print("The number is Even")
+else:
+    print("The number is Odd")
+
+for i in range(1, 11):
+    print("3 x", i, "=", 3 * i)
